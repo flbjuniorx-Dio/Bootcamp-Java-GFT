9f35f34b80dec9fec52101b4f882ebfc60918d1c
flbjuniorx <flbjuniorx@gmail.com>
2022-02-09 23:38:39 -0300

    Javadoc

--- Order/src/com/dio/base/Order.java
+++ Order/src/com/dio/base/Order.java
@@ -1,10 +1,10 @@
 package com.dio.base;
 
 /** 
- * Introdução ao Ecossistema e Documentação Java
+ * Introdução ao Ecossistema e Documentacao Java
  * 
  * Modificadores de acesso:
- * - Public: qualquer classe de qualquer pacote poderá acessar o método/atributo
+ * - Public: qualquer classe de qualquer pacote podera acessar o metodo/atributo
  * - Protected: acesso liberado para classes do mesmo pacote ou subclasse (default)
  * - Private: acesso liberado apenas para a própria classe
  * 
@@ -15,6 +15,25 @@ package com.dio.base;
  * Estrutura de condição - desvio do fluxo de execução
  * - If..Else: 
  * - Switch..Case:
+ * 
+ * Estruturas de repetição
+ * - While
+ * - Do...While
+ * - For
+ * - For each
+ * 
+ * Tags Javadoc: somente comentarios em bloco sao considerados.
+ * - @author especifica o autor da classe/método
+ * - @deprecated identifica classes/métodos obsoletos
+ *   @see associa a outras classes/métodos
+ * - @link URL para outro documento local ou remoto
+ * - @param descreve o parâmetro a ser passado para o método
+ * - @return descreve o retorno do método
+ * - @since indica desde qual versão uma classe/método foi adicionado
+ * - @throws descreve tipos de exceções que podem ser lançadas por um método
+ * - @version versão da classe/método
+ * 
+ * PS C:\Users\flbju\Dio\Bootcamp-Java-GFT\Order> javadoc.exe -d javadoc -sourcepath .\src\ -subpackages com.dio
 */
 
 public class Order {
@@ -23,6 +42,13 @@ public class Order {
 	private final Double totalValue;
 	private Double fee = 10.0;
 
+	private String[] items;
+
+	/**
+	 * Construtor da classe Order
+	 * @param code			Código do pedido
+	 * @param totalValue	Valor total (bruto)
+	 */
 	public Order(String code, Double totalValue) {
 		this.code = code;
 		this.totalValue = totalValue;
@@ -36,13 +62,35 @@ public class Order {
 				"}";
 	}
 
-	public double calculateFee() {
+	/** 
+	 * Calcula e retorna o totalValue descontado de impostos
+	 * @param fee					Taxa percentual de imposto
+	 * @return totalValue			Valor total líquido de impostos
+	 * @throws RuntimeException		Caso o valor do pedido seja negativo
+	 */
+	public double calculateFee() throws RuntimeException {
 
-		if (this.totalValue > 100) {
-			return this.totalValue * (1 - (this.fee/100));
-		} else {
-			return this.totalValue;
+		if (this.totalValue < 0 ) {
+			throw new RuntimeException("O valor do pedido nao pode ser negativo");
 		}
+		if (totalValue > 100) {
+			return totalValue * (1 - (fee/100));
+		} else {
+			return totalValue;
+		}
+		
+		/** 
+		* switch (totalValue) {
+		*	case 100:
+		*		return totalValue * (1 - (fee/100));			
+		*		break;
+			case 200:
+				return totalValue * (1 - ((fee + 1)/100);
+		*	default:
+		*		return totalValue;
+		*		break;
+		* 	}
+		*/
 	}
 
 	public Double getFee() {
@@ -50,6 +98,28 @@ public class Order {
 			return this.fee = 10.0;
 		} else {
 			return this.fee = 0.0;
+		}
+	}
+
+	public void printItems() {
+		// int i = 0;
+		// while (i < items.lenght) {
+		// 	System.out.println(items[i]);
+		// 	i++;
+		// }
+		
+		// int i = 0;
+		// do {
+		// 	System.out.println(items[i]);
+		// 	i++
+		// } while (i < items.lenght);
+
+		// for (int i = 0; i < items.lenght; i++) {
+		// 	System.out.println(items[i]);
+		// }
+
+		for (String i : items) {
+			System.out.println(i);
 		}
 	}
 
